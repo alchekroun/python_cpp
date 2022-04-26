92cd2058075782c829255a7ce3b7aad783eba740
Alexandre Chekroun <alexandre.chekroun@dauphine.eu>
2022-04-26 10:29:41 +0000

    defined python object

--- hello/hello_component.cpp
+++ hello/hello_component.cpp
@@ -1,6 +1,6 @@
 #include <pybind11/pybind11.h>
 
-char version[]="1.0";
+char version[]="2.0";
 
 char const* getVersion() {
 	return version;
@@ -10,13 +10,21 @@ char const* greet() {
   return "hello, world";
 }
 
+class Addition {
+public:
+	int operation(int x, int y) { return x + y; };
+};
+
 namespace py = pybind11;
 
 
 PYBIND11_MODULE(hello_component,greetings)
 {
-  greetings.doc() = "greeting_object 1.0";
-  
+  greetings.doc() = "greeting_object 2.0";
   greetings.def("greet", &greet, "a function saying hello");
   greetings.def("getVersion", &getVersion, "a function returning the version");
+
+  py::class_<Addition>(greetings, "Addition")
+	.def(py::init<>())
+	.def("operation", &Addition::operation);
 }
